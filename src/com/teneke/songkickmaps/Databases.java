1a7861323b5d34de4e67639712ca862d927d2f10
Emre <emre@qubitdigital.com>
2012-03-04 22:53:42 +0000

    next steps comments

--- src/com/teneke/songkickmaps/Databases.java
+++ src/com/teneke/songkickmaps/Databases.java
@@ -17,32 +17,15 @@ public class Databases {
 
 	private static QuadTree<Spot> spotDB = new QuadTree<Spot>();
 
-	public static void storeVenueToMemory(double lat, double lon, long id,
-			JSONObject data) {
-
-		Spot s = new Spot(lat, lon, id);
-		spotDB.insert(s);
-		try {
-			if (data.get("metroArea") != JSONObject.NULL) {
-				data.remove("metroArea");
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			// e.printStackTrace();
-		}
-		venueDB.put(id, data);
-	}
+	// Get instance, singleton
+	// Load from memcache the singleton
+	// Make sure there is a way to signal force refresh
+	// Make sure part of preloading takes care of this, init
 
 	public static void resetDatabases() {
 
 		venueDB = new HashMap<Long, JSONObject>();
 		spotDB = new QuadTree<Spot>();
-
-	}
-
-	public static void populateDatabases(int metroCode) throws JSONException {
-
-		populateDatabases(metroCode, 1);
 
 	}
 
@@ -54,14 +37,6 @@ public class Databases {
 	public static JSONArray venueListByGeo(Double minLat, Double maxLat,
 			Double minLon, Double maxLon) {
 
-		// if (venueDB.size() == 0) {
-		// try {
-		// // populateDatabases(24426);
-		// } catch (JSONException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// }
 		Spot min = new Spot(minLat, minLon, 1);
 		Spot max = new Spot(maxLat, maxLon, 1);
 		Interval2D range = new Interval2D(min, max);
@@ -71,42 +46,6 @@ public class Databases {
 			rv.put(venueDB.get(s.getId()));
 		}
 		return rv;
-	}
-
-	public static void populateDatabases(int metroCode, int page)
-			throws JSONException {
-
-		JSONObject data = new JSONObject(SongKickAPI.getMetroData(metroCode, page));
-		JSONObject resultsPage = data.getJSONObject("resultsPage");
-		JSONObject results = resultsPage.getJSONObject("results");
-		if (results.has("event")) {
-			JSONArray events = results.getJSONArray("event");
-			for (int i = 0; i < events.length(); i++) {
-				JSONObject event = events.getJSONObject(i);
-				JSONObject venue = event.getJSONObject("venue");
-				if (venue != JSONObject.NULL && venue.get("lat") != JSONObject.NULL
-						&& venue.get("lng") != JSONObject.NULL) {
-					long venueId = -1;
-					try {
-						venueId = venue.getLong("id");
-					} catch (Exception e) {
-						// System.err.println("Error in parsing:" + venue);
-					}
-					if (venueId != -1 && getVenue(venueId) == null) {
-						try {
-							storeVenueToMemory(venue.getDouble("lat"),
-									venue.getDouble("lng"), venue.getLong("id"), venue);
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-			page++;
-			populateDatabases(metroCode, page);
-		}
-
 	}
 
 	public static void main(String[] args) {
